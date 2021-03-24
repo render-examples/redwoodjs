a8dfc24d06af40ad890c52e395fdd67c87420e64
SEANDOUGHTY <sean@render.com>
2021-03-24 15:53:29 -0400

    creating redwood-render home page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Link, routes } from '@redwoodjs/router'
 
 const HomePage = () => {
@@ -12,6 +13,43 @@ const HomePage = () => {
         <Link to={routes.home()}>Home</Link>`
       </p>
     </>
+=======
+const HomePage = () => {
+  return (
+    <section className="temp-section">
+      <img src="images/green.svg" alt="" className="temp-img temp-img-green" />
+      <img
+        src="images/5efe8c86adc66d00f28144be_blue-red.svg"
+        alt=""
+        className="temp-img temp-img-pink"
+      />
+      <img
+        src="images/yellow-blue.svg"
+        alt=""
+        className="temp-img temp-img-yellow"
+      />
+      <a href="http://www.render.com" className="temp-logo-link w-inline-block">
+        <img
+          src="images/render-logo-color-white.svg"
+          alt=""
+          className="temp-logo"
+        />
+      </a>
+      <div className="temp-container-center">
+        <h2 className="temp-h1">
+          Hello
+          <br />
+          Redwood
+        </h2>
+        <h5 className="temp-subhead">
+          Deployed on{' '}
+          <a href="http://www.render.com" className="temp-link">
+            Render
+          </a>
+        </h5>
+      </div>
+    </section>
+>>>>>>> 169bb28 (creating redwood-render home page)
   )
 }
 
