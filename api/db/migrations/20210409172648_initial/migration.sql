729726cad820c1018a59073625aace6f4b001ab0
SEANDOUGHTY <sean@render.com>
2021-04-27 12:33:45 -0400

    adjusting config for sqlite
    - deleted migrations and changed schema.prisma to `sqlite`
    - create migrations with `yarn rw prisma migrate dev`
    - create render.yaml with `yarn rw setup deloy render -d sqlite`

--- api/db/migrations/20210409172648_initial/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Vote" (
-    "id" SERIAL NOT NULL,
-    "choice" TEXT NOT NULL,
-    "dateTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    PRIMARY KEY ("id")
-);
